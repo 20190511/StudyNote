fea12415c038f96c2be731c388d6d2e42d0b24cc
배준형 <70988272+20190511@users.noreply.github.com>
2023-02-06 14:27:08 +0900

    Create write3.c

---
+++ 2023년도/리눅스/2_파일입출력/write3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define NAME_SIZE	64
+
+typedef struct {
+	char name[NAME_SIZE];
+	int pid;
+	int salary;
+
+}ssu_employee;
+
+
+int main(int argc, char* argv[])
+{
+	ssu_employee record;
+	int fd;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "usage : %s <filename>\n", argv[0]);
+		exit(1);
+	}
+
+	//O_EXCL  : 이미 존재하면 Error처리.
+	if ((fd = open(argv[1], O_WRONLY | O_CREAT | O_EXCL, 0640)) < 0)
+	{
+		fprintf(stderr, "%d, file Error : %s\n", fd, argv[1]);
+		exit(1);
+	}
+
+	while (1)
+	{
+		printf("Enter employee name <space> salary : ");
+		scanf("%s", record.name);
+
+		if (record.name[0] == '.')
+			break;
+		
+		scanf("%d", &record.salary);
+		//getpid() :-> 프로세스 id를 받아오는 함수 (현재 프로세스 pid를 가져와서 저장).
+		record.pid = getpid();
+		printf("pid = %d\n", record.pid);
+
+		//구조체 모든 변수를 문자열화 (char*)&record (단, char*타입이 없어야함, char a[40] 이렇게 쓸 것.
+		write(fd, (char*)&record, sizeof(record));
+		printf("sizeof(char(*)struct) = %ld, %s\n",sizeof((char*)&record),(char*)&record);
+		
+	}
+	close(fd);
+	exit(0);
+}
