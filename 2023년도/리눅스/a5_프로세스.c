dfb033de6c327d4fec0f66b2bf2dc4a20377a376
배준형 <70988272+20190511@users.noreply.github.com>
2023-02-11 10:50:56 +0900

    Create a5_프로세스.c

---
+++ 2023년도/리눅스/a5_프로세스.c
@@ -0,0 +1,63 @@
+/* 0. Shell 명령어 및 Others */
+ 1. find            : 현재 할당된 메모리 값들을 볼 수 있음;
+
+
+/* 1. exit, _Exit, _exit : 프로세스 종료 함수 
+ * exit는 표준입출력 라이브러리, 종료처리부들 등을 정리하고 종료 -> 부모에게 status 전달
+ * _Exit, _exit 는 위의 부분들을 정리하지 않고 즉시 종료.
+ * status는 8비트까지만 사용하므로 : 0~255 까지의 정수를 부모프로세스에게 넘겨줄 수 있음
+ * 실제과정은 exit() 호출 후 파일 스트림을 닫고 표준입출력을 종료한 후 _exit()를 호출한다 -> 이후 운영체제가 fd를 닫고 메모리할당을 해제한다.
+ */
+#include <stdio.h>
+void exit (int status);                 // std Library, 종료처리부들 등을 정리하고 -> _exit()호출
+void _Exit (int status);                // 정리하지 않고 즉시종료 -> 부모프로세스에게 status 전달
+
+#include <unistd.h>
+void _exit (int status);                // 정리하지 않고 즉시종료 -> 부모프로세스에게 status 전달
+
+
+/* 2. atexit, on_exit() : 종료처리 과정 중 호출될 함수 
+ * etexit : void(*func)(void) 함수가 '최신에 등록된 순서' 로 호출됨.
+ *          중요한 점은 exit()는 호출할 시 무한재귀호출이 일어나므로, _exit()를 호출할 것.
+ * on_exit() : void(*func2)(int, void*) 함수를 호출 할 수 있고
+ *              arg인자로 종료과정에 메모리포인터를 전달해줄 수 있으나, 해당값이 유효해야함
+ */
+#include <stdlib.h>
+int atexit ( void(*func)(void));                    // 종료처리과정 중 함수호출 가능 (void타입, void매개변수)
+int on_exit( void(*func)(int, void*), void* arg);   // 종료처리과정 중 함수호출 가능 (void타입, int,void*매개변수), 메모리포인터  
+  return 성공시 0, 실패시 0이외의 값;
+
+
+/* 3. main(int argc, char* argv[]) 생략*/
+/* 4. malloc, calloc, realloc, free : 메모리 할당 함수 
+ *    LINUX에서 메모리는 자유리스트(free list)를 통해 적절히 포인터를 조정해서 메모리 블록을 가져온다.
+ *    만약, 메모리 풀에서 원하는 블록이 없을 경우에는 프로세스의 heap(힙) 크기를 조정할 수 있는 sbrk() 시스템콜을 하여
+ *    시스템으로부터 더 많은 메모리를 가져올 수 있다.
+ *    ★ realloc의 size는 새로 할당할 전체크기의 사이즈를 할당해준다.
+ *
+ *  <메모리 할당 시 주의할 점> 
+ * A. 이미 할당해제한 메모리 블록 (자유 메모리 풀에 있는 메모리블록) 을 다시 free를 하는 경우 
+ *     혹은 할당받지않은 메모리에 대해 free()를 하는 경우
+ *     메모리 블록 헤더를 손상시켜 치명적인 결과를 가져올 수 있으므로 주의해야한다.
+ * B. free를 제대로 하지않으면 메모리들이 프로세스 중 지속적으로 누적이되어 메모리 누출 (Memory Leak) 이 발생하여 
+ *      프로세스 실행속도에 영향을 미친다.
+ */
+#include <stdlib.h>
+void* malloc (size_t size);                         // size 공간만큼 자유메모리(free memory) 에서 메모리공간을 할당받음
+void* realloc (void* ptr, size_t newsize);          // ptr 메모리공간의 크기를 newsize로 다시 할당받음 (원본데이터는 변하지않음)
+void* calloc (size_t obj_size, size_t count);       // 개당 메모리블럭 사이즈(obj_size)를 count개수만큼 메모리공간에 할당받고 이를 다 0으로 채움.
+  return 성공시 할당된 메모리를 가리키는 포인터, 실패시 NULL;
+void free(void* ptr)l                               // 메모리 할당해제
+  
+
+/* 5. getenv, putenv, setenv, unsetenv : 환경변수 설정*/
+#include <stdlib.h>
+char *getenv (const char* name);                                    // name에 해당하는 환경변수 값을 가져옴.
+  return 성공시 name으로 주어진 이름에 해당하는 환경변수 포인터 반환, 실패시 NULL;
+int putenv (char* str);                                             // str = "name=value" 값으로 환경변수 설정
+  return 성공시 0, 실패시 0이아닌값 -> errno 설정;
+int setenv (const char *name, const char *value, int rewrite);      // name="name", value="value" 값으로 환경변수 설정, rewrite는 Overwrite 여부 확인
+  return 성공시 0, 실패시 -1 -> errno 설정;                     
+int unsetenv (const char *name);                                    // name에 해당하는 환경변수 삭제.
+  return 성공시 0, 실패시 -1 -> errno 설정;
+
